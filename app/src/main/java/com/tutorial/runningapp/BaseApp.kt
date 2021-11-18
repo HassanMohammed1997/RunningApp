c867136aaefe9320d15344e2ecc24ec72c8ad3eb
Hassan Mohammed <hassan.s.mohammed1997@gmail.com>
2021-11-18 22:11:43 +0200

    setup Hilt for database

---
+++ app/src/main/java/com/tutorial/runningapp/BaseApp.kt
@@ -0,0 +1,8 @@
+package com.tutorial.runningapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApp  : Application(){
+}
---
+++ app/src/main/java/com/tutorial/runningapp/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.tutorial.runningapp.utils
+
+object Constants {
+    const val DATABASE_NAME = "running_db"
+}
