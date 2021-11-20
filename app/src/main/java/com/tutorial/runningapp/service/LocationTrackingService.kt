4ea5dcd86637a4b89e8f213e624197ff4b6cfa1c
Hassan Mohammed <hassan.s.mohammed1997@gmail.com>
2021-11-20 17:22:28 +0200

    Tracking the user location in the background

--- app/src/main/java/com/tutorial/runningapp/service/LocationTrackingService.kt
+++ app/src/main/java/com/tutorial/runningapp/service/LocationTrackingService.kt
@@ -1,22 +1,54 @@
 package com.tutorial.runningapp.service
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
+import android.location.LocationManager
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import com.tutorial.runningapp.R
 import com.tutorial.runningapp.ui.MainActivity
 import com.tutorial.runningapp.utils.Constants
+import com.tutorial.runningapp.utils.LocationUtils
 import timber.log.Timber
+
+typealias Polyline = MutableList<LatLng>
+typealias Polyines = MutableList<Polyline>
 
 class LocationTrackingService : LifecycleService() {
     var isFirstRun = true
+
+    lateinit var fuesdLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData(false)
+        val pathPoints = MutableLiveData<Polyines>(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        fuesdLocationProviderClient = FusedLocationProviderClient(this)
+
+        isTracking.observe(this) {
+            updateLocationTracking(it)
+        }
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when (it.action) {
@@ -41,6 +73,56 @@ class LocationTrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    private fun addEmptyPolyLine() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val point = LatLng(it.latitude, it.longitude)
+            pathPoints.value?.apply {
+                last().add(point)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            if (isTracking.value!!) {
+                result.locations.let { locations ->
+                    for (location in locations) {
+                        addPathPoint(location)
+                        Timber.d("Location: $location")
+                    }
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking) {
+            if (LocationUtils.hasLocationPermission(this)) {
+                val request = LocationRequest.create().apply {
+                    interval = Constants.LOCATIONS_UPDATE_INTERVAL
+                    fastestInterval = Constants.FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+
+                fuesdLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        } else {
+            fuesdLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel(notificationManager: NotificationManager) {
         val notificationChannel = NotificationChannel(
@@ -53,6 +135,8 @@ class LocationTrackingService : LifecycleService() {
     }
 
     private fun startForegroundService() {
+        addEmptyPolyLine()
+        isTracking.postValue(true)
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
